fa5db2cd1878b313f468b07d10990df41200cee6
Chad Dumler-Montplaisir <chad.dumler.montplaisir@gmail.com>
2015-04-18 11:28:20 -0400

    Coverage #1 was not instrumenting the tests.

--- gulp/tasks/coverage.js
+++ gulp/tasks/coverage.js
@@ -6,11 +6,12 @@ gulp.task('coverage', function(cb) {
 
   gulp
     // We actually have to read the files here, streams aren't supported later.
-    .src('src/**/*.js')
+    .src(['src/**/*.js'])
     .pipe(istanbul({includeUntested: true}))
+    .pipe(istanbul.hookRequire())
     .on('finish', function() {
       gulp
-        .src('test/**/*_spec.js', {read: false})
+        .src(['test/**/*_spec.js'], {read: false})
         .pipe(mocha({reporter: 'min'}))
         .pipe(istanbul.writeReports())
         .on('end', cb);
